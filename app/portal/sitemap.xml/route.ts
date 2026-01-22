24e3c29270a4f3b68e8a8b14cca95645b689a56f
Flavio F <flavio.win@gmail.com>
2026-01-22 12:05:42 -0300

    Add test revalidate

--- app/portal/sitemap.xml/route.ts
+++ app/portal/sitemap.xml/route.ts
@@ -6,7 +6,7 @@ const xml = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <!-- Página inicial -->
       <url>
-        <loc>https://portal.flavio-franco-tester.fun</loc>
+        <loc>https://portal.flavio-franco-tester.funxxx</loc>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
       </url>
