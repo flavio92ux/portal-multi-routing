941fc2b4b052cb93624b8aa1805ed983255283cc
Flavio F <flavio.win@gmail.com>
2026-01-22 10:33:35 -0300

    Mock response

--- app/portal/sitemap.xml/route.ts
+++ app/portal/sitemap.xml/route.ts
@@ -1,3 +1,12 @@
+import { NextResponse } from 'next/server';
+
 export async function GET() {
-    
+
+return new NextResponse('ok', {
+    headers: {
+      'Content-Type': 'application/text',
+      'Cache-Control': 'public, max-age=3600, s-maxage=3600, stale-while-revalidate=86400',
+    },
+  });
+
 }
