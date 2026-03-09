bc6fa12a65d7805a8616adbcf5069dfd3df067ca
Flavio F <flavio.win@gmail.com>
2026-03-09 16:45:40 -0300

    Reduce to 500ms

--- components/templates/header/BandHeader.tsx
+++ components/templates/header/BandHeader.tsx
@@ -19,7 +19,7 @@ export function BandHeader({ headerData }: BandHeaderProps) {
       script.src = 'https://securepubads.g.doubleclick.net/tag/js/gpt.js';
       script.async = true;
       document.head.appendChild(script);
-    }, 2500);
+    }, 500);
 
     return () => clearTimeout(timer);
   }, []);
