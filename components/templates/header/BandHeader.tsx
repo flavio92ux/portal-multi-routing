0090ec70359ecf3037397051483ece40bbfba346
Flavio F <flavio.win@gmail.com>
2026-03-09 16:49:03 -0300

    increase

--- components/templates/header/BandHeader.tsx
+++ components/templates/header/BandHeader.tsx
@@ -19,7 +19,7 @@ export function BandHeader({ headerData }: BandHeaderProps) {
       script.src = 'https://securepubads.g.doubleclick.net/tag/js/gpt.js';
       script.async = true;
       document.head.appendChild(script);
-    }, 500);
+    }, 5000);
 
     return () => clearTimeout(timer);
   }, []);
